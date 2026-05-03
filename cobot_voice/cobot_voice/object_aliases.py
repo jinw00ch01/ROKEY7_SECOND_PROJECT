d531c6ed2d29961ea51f9a4dc5247d4364424141
jin_w00_ch01 <herofactorycjw1998@gmail.com>
2026-05-03 13:51:13 +0900

    1st commit: data recording modified

---
+++ cobot_voice/cobot_voice/object_aliases.py
@@ -0,0 +1,12 @@
+# Canonical nut name mapping (Korean/English variants → canonical English name)
+NUT_KEYWORD_MAP = {
+    "호두": "walnut",       "walnuts": "walnut",      "walnut": "walnut",
+    "아몬드": "almond",     "almonds": "almond",      "almond": "almond",
+    "캐슈넛": "cashew",     "cashews": "cashew",      "cashew": "cashew",
+    "피스타치오": "pistachio", "pistachios": "pistachio", "pistachio": "pistachio",
+    "마카다미아": "macadamia", "macadamia": "macadamia",
+    "건포도": "raisin",     "raisins": "raisin",      "raisin": "raisin",
+}
+
+# All valid canonical nut names
+NUT_NAMES = {"walnut", "almond", "cashew", "pistachio", "macadamia", "raisin"}
--- cobot_voice/setup.py
+++ cobot_voice/setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import glob
 
 package_name = 'cobot_voice'
 
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/resource', glob.glob('resource/*')),
+        ('share/' + package_name + '/resource', glob.glob('resource/.env')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
